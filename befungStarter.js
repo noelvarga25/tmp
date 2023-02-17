227670194f44ce4820875ac639f044353d0bdf7e
Noel Varga <noel.varga@epitech.eu>
2023-02-17 17:37:57 +0100

    [FIX]

--- befungStarter.js
+++ befungStarter.js
@@ -2,10 +2,8 @@ const Befunge = require('befunge93');
 const fs = require('fs')
 
 let befunge = new Befunge();
-let rd = process.argv[2];
 
-befunge.run(fs.readFileSync(fileToRead).toString());
-
-befunge.onOutput = (output) => {
-    console.log(output);
-};
+befunge.run(fs.readFileSync(process.argv[2]).toString())
+    .then((output) => {
+        process.stdout.write(output);
+    });
