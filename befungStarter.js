14dafb845039fd7f0bf87ffb4c896ad86083a094
Noel Varga <noel.varga@epitech.eu>
2023-02-17 17:24:13 +0100

    [FIX]

--- befungStarter.js
+++ befungStarter.js
@@ -1,19 +1,11 @@
-const befunge93 = require('befunge93');
-const fs = require('fs');
+const Befunge = require('befunge93');
+const fs = require('fs')
 
-// Get the filename from the command-line arguments
-const filename = process.argv[2];
+let befunge = new Befunge();
+let rd = process.argv[2];
 
-// Read the contents of the file
-fs.readFile(filename, 'utf8', function(err, contents) {
-  if (err) {
-    console.error(`Error reading file ${filename}: ${err.message}`);
-    return;
-  }
+befunge.run(fs.readFileSync(fileToRead).toString());
 
-  // Execute the contents of the file using befunge93
-  const result = befunge93(contents);
-
-  // Print the output of the program
-  console.log(result);
-});
+befunge.onOutput = (output) => {
+    console.log(output);
+};
